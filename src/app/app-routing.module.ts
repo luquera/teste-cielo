2e92cf85d987d6dac8364837dd6a8937299526e6
Lucas Camargo <lucashsc31@gmail.com>
2019-07-16 06:06:10 -0300

    fix router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LancamentosComponent } from './pages/lancamentos/lancamentos.component';
-import { TesteComponent } from './teste/teste.component';
-
 
 const routes: Routes = [
-  { path: '', redirectTo: 'teste', pathMatch: 'full' },
   // { path: 'lancamentos', loadChildren: () => import('./pages/lancamentos/lancamentos.module').then(m => m.LancamentosModule) },
   { path: 'lancamentos', loadChildren: './pages/lancamentos/lancamentos.module#LancamentosModule' },
-  { path: 'teste', component: TesteComponent }
 ];
 
 @NgModule({
